eb6977c2db44fbda9f52c8c8d62b1abceae43c05
Luis Matus <luis.matus.jr@gmail.com>
2024-07-16 14:01:11 -0600

    Terminando las preparaciones previas a redux

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,13 +1,28 @@
-import { Typography } from "@mui/material";
+import { IconButton, Typography } from "@mui/material";
 import { JournalLayout } from "../Layout/JournalLayout";
-import { NoteView } from "../views";
+import { NoteView, NothingSelectedView } from "../views";
+import { AddOutlined } from "@mui/icons-material";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
       {/* <Typography variant="h4">Journal Page</Typography> */}
-      {/* <NothingSelectedView /> */}
-      <NoteView />
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+      {/* Botón flotante */}
+      <IconButton
+        size="large"
+        sx={{
+          color: "white",
+          backgroundColor: "error.main",
+          ":hover": { backgroundColor: "error.main", opacity: 0.9 },
+          position: "fixed",
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
   );
 };
